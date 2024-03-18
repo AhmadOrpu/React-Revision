96317afcccd10f2afb964909a5fde0b5549e23c4
AhmadOrpu <150029154+AhmadOrpu@users.noreply.github.com>
2024-03-18 22:35:31 +0600

    Update Footer.jsx

--- vite-project/src/Components/Footer.jsx
+++ vite-project/src/Components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div>
             <p>All Rights Reserved@2024 And Author Orpu</p>
+            <p> Orpu Vaiya Has Made Changed</p>
         </div>
     );
 };
